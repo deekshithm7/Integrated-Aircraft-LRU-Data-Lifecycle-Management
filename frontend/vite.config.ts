e2df658420561db98f9c73a1fcf477d0a5c18f0c
DEEKSHITH M <deekshith7208@gmail.com>
2025-07-28 21:26:36 +0530

    added tailwind and removed unwanted files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   server: {
     host: true, // Needed for Docker to expose the port
     port: 3000, // Explicitly set the port to 3000
   },
 })
+
